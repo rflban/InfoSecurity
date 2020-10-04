31bd54730d865c9a4c4c5f99079875c093e6df69
rflban <nabiev.alsn@yandex.ru>
2020-10-04 22:32:28 +0300

    complete installer

--- lab_01/installer/main.c
+++ lab_01/installer/main.c
@@ -1,24 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ifaddrs.h>
 #include <netpacket/packet.h>
 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "utils.h"
 
-int main(void)
+#define KEY_PATHTO ".local/"
+#define KEY_DIRECTORY "infosecurity_lab01/"
+#define KEY_FILENAME  "key.bin"
+
+int install();
+int uninstall();
+
+int main(int argc, char **argv)
+{
+    if (argc > 1 && argv[1][0] == 'u')
+        return uninstall();
+    else
+        return install();
+
+    return 0;
+}
+
+int install()
 {
     struct mac_addrs *maddrs;
+    char back[128];
+    FILE *key_file;
+
+    getcwd(back, sizeof(back));
+
+    chdir(getenv("HOME"));
+    chdir(KEY_PATHTO);
+    mkdir(KEY_DIRECTORY, 0777);
+    chdir(KEY_DIRECTORY);
 
     maddrs = get_mac_addrs();
+    key_file = fopen(KEY_FILENAME, "w");
 
-    for (struct mac_addrs *ma = maddrs; ma; ma = ma->next)
-    {
-        printf("%s: ", ma->ifa_name);
-        
-        for (int idx = 0; idx < ma->alen; ++idx)
-            printf("%x%c", ma->addr[idx], (idx >= ma->alen-1 ? '\n' : ':'));
-    }
+    for (int idx = 0; idx < maddrs->alen; ++idx)
+        fprintf(key_file, "%x", maddrs->addr[idx]);
 
+    fclose(key_file);
     free_mac_addrs(maddrs);
+    chdir(back);
+
+    return 0;
+}
+
+int uninstall()
+{
+    char back[128];
+
+    getcwd(back, sizeof(back));
+
+    chdir(getenv("HOME"));
+    chdir(KEY_PATHTO KEY_DIRECTORY);
+
+    remove(KEY_FILENAME);
+    chdir("..");
+    rmdir(KEY_DIRECTORY);
+
+    chdir(back);
 
     return 0;
 }
